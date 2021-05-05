03e0b035ad8e72aad117bc2905ef61dfeb905b3f
unknown <s115345@ap.be>
2021-05-06 00:54:59 +0200

    LerenGebruikenPOST

---
+++ REST-API-Make-Directory.ps1
@@ -0,0 +1,15 @@
+﻿$Token ='s115455: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'
+$Base64Token = [System.Convert]::ToBase64String([char[]]$Token);
+
+$Headers = @{
+
+    Authorization = 'Basic {0}' -f $Base64Token;
+    };
+
+$Body =@{
+    name = 'Test';
+    description ='This was created from Powershell';
+    } | ConvertTo-Json
+
+
+Invoke-RestMethod -Headers $Headers -Uri https://api.github.com/user/repos -Body $Body -Method Post 
