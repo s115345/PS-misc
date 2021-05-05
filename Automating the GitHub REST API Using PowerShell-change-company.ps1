564ef3a1afd81b3cb317427f10bffb521f0b961d
unknown <s115345@ap.be>
2021-05-05 23:43:08 +0200

    verander-objects

---
+++ Automating the GitHub REST API Using PowerShell-change-company.ps1
@@ -0,0 +1,15 @@
+﻿$Token ='s115455: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'
+$Base64Token = [System.Convert]::ToBase64String([char[]]$Token);
+
+$Headers = @{
+
+    Authorization = 'Basic {0}' -f $Base64Token;
+    };
+
+$Body =@{
+    Company = 'Ap Hogeschool-antwerpen';
+    } | ConvertTo-Json
+
+
+Invoke-RestMethod -Headers $Headers -Uri https://api.github.com/user -Body $Body -Method Patch 
+
